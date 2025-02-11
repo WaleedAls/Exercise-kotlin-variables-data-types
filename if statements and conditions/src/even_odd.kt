adc7d6204e70171270fa6f123e718f57054bef39
WaleedAls <swyd16@gmail.com>
2025-02-11 21:25:36 +0300

    MyFirst coding

---
+++ if statements and conditions/src/even_odd.kt
@@ -0,0 +1,10 @@
+fun main () {
+
+var num: Int = 3
+
+if ( num % 2 == 0 ) {
+
+    println(" the number is even")
+} else
+    println("the number is odd")
+}
---
+++ if statements and conditions/src/if_else_if.kt
@@ -0,0 +1,13 @@
+fun main () {
+    var num: Int = 0
+
+    if ( num > 0 ) {
+        println(" the number is positive")
+    } else if ( num < 0 ){
+        println("The number is negative")
+    } else if (num == 0){
+
+        println("The number is Zero")
+    }
+
+}
