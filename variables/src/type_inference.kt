7c5bbac736bbd171c5fcd41533c9302830312e77
WaleedAls <swyd16@gmail.com>
2025-02-11 19:08:57 +0300

    MyFirst coding

---
+++ variables/src/type_inference.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var city  = "New York"
+    var temperature = 22.5
+
+
+
+    println (city.javaClass.kotlin)
+    println (temperature.javaClass.kotlin)
+
+}
---
+++ variables/src/val_vs_var.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var score: Int = 10
+    score = 20
+    val greeting: String = "Hello, Kotlin!"
+
+}
