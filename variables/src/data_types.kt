f406a1c08cd1a82cfe8fec14437499a88db4c603
WaleedAls <swyd16@gmail.com>
2025-02-11 20:45:14 +0300

    MyFirst coding

---
+++ variables/src/data_types.kt
@@ -0,0 +1,8 @@
+fun main () {
+    var temperature: Double = 36.5
+    var raining: Boolean = false
+    var letter: Char = 'A'
+    println (" the temperature is $temperature degree")
+    println (" Is it raining? $raining")
+    println (" the first letter of my name is $letter")
+}
