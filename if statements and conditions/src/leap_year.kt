7b3fb4039ba56e43ff6cbf911af3648cba472824
WaleedAls <swyd16@gmail.com>
2025-02-12 16:15:47 +0300

    MyFirst coding

---
+++ if statements and conditions/src/leap_year.kt
@@ -0,0 +1,15 @@
+fun main () {
+
+    var year: Int = 2020
+    var zero: Int = 0
+
+    if (year % 4 == zero) {
+
+        println (" Leap year")
+
+    } else {
+        println (" Not Leap year")
+    }
+
+
+}
