62806206e4319cb0fedb637450272ab246d6d434
WaleedAls <swyd16@gmail.com>
2025-02-12 17:05:24 +0300

    MyFirst coding

---
+++ Functions/src/currentTemperature.kt
@@ -0,0 +1,15 @@
+fun main () {
+
+return currentTemperature()
+
+}
+
+fun currentTemperature () {
+
+    var morningTemperature: Int = 25
+    var afternoonTemperature: Int = 40
+    var Average: Int = morningTemperature + afternoonTemperature
+    var averageTemp = Average/ 2
+ println(" Average temperature: $averageTemp")
+
+}
