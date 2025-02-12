b899ce882f9275a6de4117814cee5534074e7078
WaleedAls <swyd16@gmail.com>
2025-02-12 14:25:54 +0300

    MyFirst coding

---
+++ if statements and conditions/src/largest_number.kt
@@ -0,0 +1,13 @@
+fun main (){
+    var a: Int = 25
+    var b: Int = 10
+    var c: Int = 15
+
+   if ( a > b && a > c ) {
+
+       println("The largest number is $a ")
+   } else if (b > a && b > c) {
+       println("The largest number is $b")
+   } else
+       println("The largest number is $c")
+}
