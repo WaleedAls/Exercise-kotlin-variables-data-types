270b824f8621013461ec8c513393ce7d70adf0da
WaleedAls <swyd16@gmail.com>
2025-02-16 01:18:47 +0300

    Lambdas

---
+++ Lambda/src/Lambda with List Transformation.kt
@@ -0,0 +1,13 @@
+val transformation = {n: Int -> n * 2 }
+
+
+
+fun main (){
+
+    val numbers = mutableMapOf(1 to 1, 2 to 2,3 to 3,4 to 4, 5 to 5, 6 to 6,7 to 7,8 to  8,9 to  9,10 to 10)
+    val value = (numbers.values)
+    println(value)
+
+    println(transformation(value.))
+
+}
