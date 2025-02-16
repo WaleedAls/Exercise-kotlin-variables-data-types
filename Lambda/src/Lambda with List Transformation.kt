82a844a7181475dd392ff759ad0709a3c83f693d
WaleedAls <swyd16@gmail.com>
2025-02-16 17:45:39 +0300

    Lambdas

--- Lambda/src/Lambda with List Transformation.kt
+++ Lambda/src/Lambda with List Transformation.kt
@@ -1,19 +1,7 @@
-import kotlin.math.ln1p
-
 val transformation = { n: Int -> n * 2 }
 
-
 fun main (){
-
-    val numbers = mutableMapOf(1 to 1, 2 to 2,3 to 3,4 to 4, 5 to 5, 6 to 6,7 to 7,8 to  8,9 to  9,10 to 10)
-    val value = (numbers.values)
-    println(value)
-
-    for (n1 in numbers) {
-        println(n1)
-     //   return n1
+    val numbers = mutableListOf( 1,2,3,4,5,6,7,8,9,10)
+   var n = numbers.map(transformation)
+  println(n)
     }
-
-    println(transformation)
-
-}
